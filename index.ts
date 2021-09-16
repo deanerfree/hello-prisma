15463aee4e030c8c071377ebf3bb0e02fa8e6019
Kurt Ball <kurtisdball@hotmail.com>
2021-09-16 15:21:23 -0600

    done

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     posts: {
+  //       create: { title: 'Hello World' },
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.dir(allUsers, { depth: null})
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
